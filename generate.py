bfb3ed15c6830a7ff3b4f456b74b73104457c3f5
William Stein <wstein@sagemath.com>
2023-02-26 10:39:41 -0800

    include all tickets pages

--- generate.py
+++ generate.py
@@ -23,13 +23,13 @@ def redirect(n):
     <p>This ticket is now <a href="https://github.com/sagemath/sage/issues/{n}">hosted on GitHub</a>.</p>
 
 <p>
-    There is also <a href="https://web.archive.org/web/20230130064641/https://trac.sagemath.org/ticket/{n}">a read-only archive of the original trac ticket at trac.sagemath.org</a>.
+    There is also <a href="https://web.archive.org/web/20230130064641/https://trac.sagemath.org/ticket/{n}">a read-only archive at web.archive.org</a>.
     </p>
   </body>
 </html>
 """
 
 
-for n in range(1, 5):
+for n in range(1, 35200):
     mkdir(f'ticket/{n}')
     open(f"ticket/{n}/index.html", 'w').write(redirect(n))
